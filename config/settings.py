43e355562ebb923f50aeef7dda50e7a9808675bc
Sai Krishnan RU <saikrishnan185@gmail.com>
2025-12-28 20:41:17 +0530

    Add authentication and role system

--- config/settings.py
+++ config/settings.py
@@ -37,6 +37,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    # Local apps
+    'accounts.apps.AccountsConfig',
+    'orders',
+    'inventory',
+    'tasks',
+    'payments',
+    'dashboard',
 ]
 
 MIDDLEWARE = [
