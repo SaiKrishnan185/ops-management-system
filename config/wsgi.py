da247b05f9874269445b09a452fa11dc0998eb19
Sai Krishnan RU <saikrishnan185@gmail.com>
2025-12-30 09:48:02 +0530

    Adding admin to datatbase

--- config/wsgi.py
+++ config/wsgi.py
@@ -1,14 +1,45 @@
+"""
+WSGI config for config project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
+"""
+
 import os
+
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ops_system.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
 application = get_wsgi_application()
 
-# 🚨 DEBUG PRINT
-print("🔥 WSGI LOADED")
 
-if os.getenv("CREATE_ADMIN") == "1":
-    print("🔥 CREATE_ADMIN detected")
-    from django.core.management import call_command
-    call_command("create_admin")
+from django.core.management.base import BaseCommand
+from django.contrib.auth.models import User
+import os
+
+class Command(BaseCommand):
+    help = "Create initial admin user if not exists"
+
+    def handle(self, *args, **kwargs):
+        username = os.getenv("DJANGO_ADMIN_USERNAME")
+        email = os.getenv("DJANGO_ADMIN_EMAIL")
+        password = os.getenv("DJANGO_ADMIN_PASSWORD")
+
+        if not username or not password:
+            self.stdout.write("Admin env vars not set. Skipping.")
+            return
+
+        if User.objects.filter(username=username).exists():
+            self.stdout.write("Admin user already exists.")
+            return
+
+        User.objects.create_superuser(
+            username=username,
+            email=email,
+            password=password
+        )
+
+        self.stdout.write("Admin user created successfully.")
